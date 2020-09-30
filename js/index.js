210cd06eb6328d3c6ddeb4f26139c29b291d4a35
Kuba <jameshopegit@gmail.com>
2020-09-30 23:57:25 +0200

    Upgraded initial animation - picked version 3 => rotate later

--- js/index.js
+++ js/index.js
@@ -13,20 +13,39 @@ const firstSlider = {
     loop: true,
     mousewheel: true,
     reverseDirecton: false,
+    speed: 800,
 };
+
 var swiper = new Swiper('.swiper-container', firstSlider);
 
-var evtLeft = new KeyboardEvent('keydown', {'keyCode':37}); 
-var evtRight = new KeyboardEvent('keydown', {'keyCode':39}); 
-function turnLeft() {
-    document.dispatchEvent (evtLeft); 
-}
-function turnRight() {
-    document.dispatchEvent (evtRight); 
-}
-setTimeout(turnRight,3800)
-setTimeout(turnLeft,4200)
+//VERSION 1 => BOLD
 
+// var evtLeft = new KeyboardEvent('keydown', {'keyCode':37}); 
+// var evtRight = new KeyboardEvent('keydown', {'keyCode':39}); 
+// function turnLeft() {
+//     document.dispatchEvent (evtLeft); 
+// }
+// function turnRight() {
+//     document.dispatchEvent (evtRight); 
+// }
+// setTimeout(turnRight,3800)
+// setTimeout(turnLeft,4200)
 
+//VERSION 2 => ROTATE FIRST
+
+// swiper.slideNext(2000);
+// swiper.once('slideNextTransitionEnd',function(){
+//     swiper.removeSlide(0);
+//     })    
+
+//VERSION 3 => ROTATE LATER
+
+const check = setTimeout(mySlide, 4000);
+swiper.once('transitionStart', function(){
+    clearInterval(check);
+})
+function mySlide(){
+    swiper.slideNext(2000);
+}
 
 
