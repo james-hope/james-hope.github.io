6612c6e2a589ecf30df072c80074e3b2e5a3c384
Kuba <jameshopegit@gmail.com>
2020-09-30 21:09:39 +0200

    Added index.js

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const firstSlider = {
+    // autoplay: {
+    //     delay: 8000,
+    //     disableOnInteraction: false,
+    // },
+    cubeEffect: {
+        shadowOffset: 150,
+        shadowScale: '1.1',
+    },
+    effect:'cube',
+    grabCursor: true,
+    keyboard: true,
+    loop: true,
+    mousewheel: true,
+    reverseDirecton: false,
+};
+var swiper = new Swiper('.swiper-container', firstSlider);
+
+var evtLeft = new KeyboardEvent('keydown', {'keyCode':37}); 
+var evtRight = new KeyboardEvent('keydown', {'keyCode':39}); 
+function turnLeft() {
+    document.dispatchEvent (evtLeft); 
+}
+function turnRight() {
+    document.dispatchEvent (evtRight); 
+}
+setTimeout(turnRight,3800)
+setTimeout(turnLeft,4200)
+
+
+
+
